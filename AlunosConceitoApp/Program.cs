0447e3bdfebb31d0ce17b7ca4e64305614726cf0
Pedro Abreu <pedroliveirabreu@gmail.com>
2021-03-22 20:07:23 -0300

    console aplication initialized

---
+++ AlunosConceitoApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlunosConceitoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
