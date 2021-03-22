afb9f30263373d59ec5bde5813e0589c6e9402ad
Pedro Abreu <pedroliveirabreu@gmail.com>
2021-03-22 20:20:52 -0300

    options and chosen option catch done

--- AlunosConceitoApp/Program.cs
+++ AlunosConceitoApp/Program.cs
@@ -6,7 +6,38 @@ namespace AlunosConceitoApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Setting options
+
+            Console.WriteLine("Escolha a opção desejada:");
+            Console.WriteLine("1 - Cadastrar novo aluno");
+            Console.WriteLine("2 - Listar alunos");
+            Console.WriteLine("3 - Calcular média geral dos alunos");
+            Console.WriteLine("(X) Sair");
+            Console.WriteLine();
+
+
+            //reading the chosen option
+
+            string chosenOption = Console.ReadLine();
+
+            while(chosenOption.ToUpper() != "X"){
+                switch(chosenOption){
+                    case "1":
+                        //To do?: add alumn
+                        break;
+
+                    case "2":
+                        //To do?: list alumns
+                        break;
+                        
+                    case "3":
+                        //To do?: calculate class average
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
         }
     }
 }
